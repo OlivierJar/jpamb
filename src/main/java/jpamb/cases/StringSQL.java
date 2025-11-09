62e2d800457f09c12537db8010bfe43190a54b69
hananinas <hanani.nas@gmail.com>
2025-11-09 21:44:16 +0100

    Add StringSQL class with test cases for SQL injection detection

---
+++ src/main/java/jpamb/cases/StringSQL.java
@@ -0,0 +1,47 @@
+package jpamb.cases;
+
+import jpamb.utils.Case;
+
+/**
+ * Test cases i made to check SQL injection
+ */
+public class StringSQL {
+
+    @Case("(\"hello\") -> ok")
+    public static void safeString(String str) {
+        String query = "SELECT * FROM users WHERE id = 1";
+    }
+
+    @Case("(\"admin\") -> ok")
+    public static void concatenateStrings(String str1) {
+        String str2 = "world";
+        String result = str1 + " " + str2;
+    }
+
+    @Case("(\"test\") -> ok")
+    public static void substringOperation(String str) {
+        // Extract substring
+        if (str.length() > 2) {
+            String sub = str.substring(0, 2);
+        }
+    }
+
+    @Case("() -> ok")
+    public static void stringBuilder() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello");
+        sb.append(" ");
+        sb.append("World");
+        String result = sb.toString();
+    }
+
+    @Case("(\"value\") -> ok")
+    public static void stringComparison(String str) {
+        assert !str.equals("different");
+    }
+
+    public static String vulnerableQuery(String userInput) {
+        String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+        return query;
+    }
+}
