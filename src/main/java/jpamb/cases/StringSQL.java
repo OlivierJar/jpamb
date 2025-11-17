44ea23f3832d0717765a3b1c95bf7feb7e56713f
hananinas <hanani.nas@gmail.com>
2025-11-17 20:09:25 +0100

    Add unit tests for String Aware Interpreter and enhance StringSQL functionality

--- src/main/java/jpamb/cases/StringSQL.java
+++ src/main/java/jpamb/cases/StringSQL.java
@@ -45,4 +45,46 @@ public class StringSQL {
         String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
         return query;
     }
+
+    @Case("(\"hello\", 1) -> 'e'")
+    @Case("(\"hello\", -1) -> StringIndexOutOfBoundsException")
+    @Case("(\"hello\", 5) -> StringIndexOutOfBoundsException")
+    @Case("(null, 0) -> NullPointerException")
+    public static char testCharAt(String str, int index) {
+        if (str == null) {
+            throw new NullPointerException("String is null");
+        }
+        if (index < 0 || index >= str.length()) {
+            throw new StringIndexOutOfBoundsException("Index out of bounds");
+        }
+        return str.charAt(index);
+    }
+
+    @Case("(\"hello\", \"world\") -> \"helloworld\"")
+    @Case("(\"\", \"world\") -> \"world\"")
+    @Case("(\"hello\", \"\") -> \"hello\"")
+    @Case("(\"\", \"\") -> \"\"")
+    @Case("(null, \"world\") -> NullPointerException")
+    @Case("(\"hello\", null) -> NullPointerException")
+    public static String concatenateStrings(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            throw new NullPointerException("One of the strings is null");
+        }
+        return str1 + str2;
+    }
+
+    @Case("(\"hello world\", \"world\") -> true")
+    @Case("(\"hello world\", \"hello\") -> true")
+    @Case("(\"hello world\", \"\") -> true")
+    @Case("(\"hello world\", \"test\") -> false")
+    @Case("(\"\", \"\") -> true")
+    @Case("(\"\", \"test\") -> false")
+    @Case("(null, \"test\") -> NullPointerException")
+    @Case("(\"hello world\", null) -> NullPointerException")
+    public static boolean containsSubstring(String str, String substring) {
+        if (str == null || substring == null) {
+            throw new NullPointerException("One of the strings is null");
+        }
+        return str.contains(substring);
+    }
 }
